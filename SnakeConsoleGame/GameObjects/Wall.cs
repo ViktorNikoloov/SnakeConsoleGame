aaa13b05a9f39e5351f3e0159c188090e1815fce
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-07 01:13:55 +0200

    Add Point and Wall classes int GameObjects folder

---
+++ SnakeConsoleGame/GameObjects/Wall.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SnakeConsoleGame.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char WallSymbol = '\u25A0';  //■
+       
+        public Wall(int leftX, int topY) 
+            : base(leftX, topY)
+        {
+            SetHorizontalLine(0);
+            SetHorizontalLine(TopY - 1);
+
+            SetVerticallLine(0);
+            SetVerticallLine(LeftX);
+        }
+
+        private void SetHorizontalLine(int topY)
+        {
+            for (int leftX = 0; leftX < this.LeftX; leftX++)
+            {
+                Draw(leftX, topY, WallSymbol);
+            }
+        }
+
+        private void SetVerticallLine(int leftX)
+        {
+            for (int topY = 0; topY < this.TopY; topY++)
+            {
+                Draw(leftX, topY, WallSymbol);
+            }
+        }
+    }
+}
--- SnakeConsoleGame/StartUp.cs
+++ SnakeConsoleGame/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿namespace SimpleSnake
 {
+    using SnakeConsoleGame.GameObjects;
     using Utilities;
 
     public class StartUp
@@ -7,6 +8,18 @@
         public static void Main()
         {
             ConsoleWindow.CustomizeConsole();
+
+            //Point point = new Point(0, 0);
+
+            //point.Draw('@');
+            //point.Draw(1, 1, '1');
+            //point.Draw(2, 2, '2');
+            //point.Draw(0, 1, '0');
+
+
+            Wall wall = new Wall(20, 15);
+
+
         }
     }
 }
