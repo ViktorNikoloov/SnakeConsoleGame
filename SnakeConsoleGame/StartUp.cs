39da5121207d09992e257fd9c4eb6d8cf509e6e4
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-09 02:10:07 +0200

    Modify StartUp.cs

--- SnakeConsoleGame/StartUp.cs
+++ SnakeConsoleGame/StartUp.cs
@@ -1,7 +1,7 @@
 ﻿namespace SimpleSnake
 {
     using SnakeConsoleGame.GameObjects;
-    using SnakeConsoleGame.GameObjects.Food;
+    using SnakeConsoleGame.GameObjects.Foods;
     using System.Collections.Generic;
     using Utilities;
 
@@ -10,11 +10,25 @@
         public static void Main()
         {
             ConsoleWindow.CustomizeConsole();
-
+            Food
             Wall wall = new Wall(60, 20);
 
             Food food = new FoodHash(wall);
             food.SetRandomPosition(new Queue<Point>());
+
+            Snake snake = new Snake(1, 1);
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(1,0));
+            snake.isMoving(new Point(0,1));
+            snake.isMoving(new Point(0,1));
+            snake.isMoving(new Point(0,1));
+            snake.isMoving(new Point(0,1));
+            snake.isMoving(new Point(0,1));
+
         }
     }
 }
