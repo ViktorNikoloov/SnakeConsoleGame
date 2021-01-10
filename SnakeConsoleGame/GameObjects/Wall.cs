bf7e5f87642c25888457cc7927842081a23e4a8f
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-01-10 02:21:05 +0200

    Fix Wall borders

--- SnakeConsoleGame/GameObjects/Wall.cs
+++ SnakeConsoleGame/GameObjects/Wall.cs
@@ -1,13 +1,27 @@
 ﻿namespace SnakeConsoleGame.GameObjects
 {
     using System;
+    using System.Collections.Generic;
 
     public class Wall : Point
     {
         private const char WallSymbol = '\u25A0';  //■
+
+        private int playerPoints;
        
         public Wall(int leftX, int topY) 
             : base(leftX, topY)
+        {
+            InitializeWallBorders();
+        }
+
+        public bool IsPointOfWall(Point snake)
+            => snake.LeftX == 0
+            || snake.LeftX == LeftX
+            || snake.TopY == 0
+            || snake.TopY == TopY - 1;
+
+        private void InitializeWallBorders()
         {
             SetHorizontalLine(0);
             SetHorizontalLine(TopY - 1);
@@ -31,5 +45,18 @@
                 Draw(left, topY, WallSymbol);
             }
         }
+
+        public void Addpoints(Queue<Point> snakeElements)
+        => playerPoints = snakeElements.Count - 6;
+        
+        public void PlayerInfo()
+        {
+            Console.SetCursorPosition(LeftX + 3, 0);
+            Console.Write($"Player points: {this.playerPoints}");
+
+            Console.SetCursorPosition(LeftX + 3, 1);
+            Console.Write($"Player level: {this.playerPoints / 10}");
+        }
+
     }
 }
